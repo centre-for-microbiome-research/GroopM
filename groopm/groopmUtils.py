422a0be0984567b6cbbe4dcfee72321b335dbd97
Michael.Imelfort <mike@mikeimelfort.com>
2012-07-13 14:00:27 +1000

    Added contig parser

--- groopm/groopmUtils.py
+++ groopm/groopmUtils.py
@@ -47,6 +47,7 @@ import pysam
 import csv
 import os
 import numpy as np
+import string
 
 __author__ = "Michael Imelfort"
 __copyright__ = "Copyright 2012"
@@ -58,9 +59,8 @@ __email__ = "mike@mikeimelfort.com"
 __status__ = "Development"
 
 ###############################################################################
-# CLASSES
+# MAIN CLASS
 ###############################################################################
-
 class GMProj:
     """main object to poke when doing gm work"""
     def __init__(self):
@@ -69,87 +69,28 @@ class GMProj:
         self.dbName = "unset"
         self.contigsFile = "unset"
         self.bamFiles = None
+        self.bamColNames = []
         self.numContigs = -1
-        self.numBamFiles = -1
         
     def loadDB(self, fileName):
         pass
     
     def createDB(self, bamFiles, contigs, profileVals, dbName):
-        """Main wrapper for parsing all the files in"""
+        """Main wrapper for parsing all input files"""
         # load all the passed vars
         self.dbName = dbName
         self.contigsFile = contigs
-        self.bamFiles = str.split(bamFiles)
-        self.numBamFiles = len(self.bamFiles)
+        self.bamFiles = bamFiles.split(",")
         self.profileVals = profileVals
         
-        try:
-            self.conParser = ContigParser(self.contigsFile)
-        except:
-            print "Could not create ContigParser:", sys.exc_info()[0]
-        try:
-            self.bamParser = BamParser(self.bamFiles)
-        except:
-            print "Could not create BamParser:", sys.exc_info()[0]
-        
-        # parse bam files
-        do_continue = False
-        try:
-            do_continue = self.bamParser.parse(self.dbName)
-        except:
-            print "Error parsing bam files:", sys.exc_info()[0]
-        
-        # parse contigs 
-        if(do_continue):
-            try:
-                self.numContigs = self.conParser.parse(self.dbName)
-            except:
-                print "Error parsing contigs:", sys.exc_info()[0]
-        
-        if(self.numContigs == -1):
-            print "Something went wrong while parsing contigs"
-            return False
-        
-        self.bamParser.dumpCovTable(self.dbName)
+        self.conParser = ContigParser()
+        self.kse = KmerSigEngine(4)
+        self.bamParser = BamParser()
 
-class ContigParser:
-    """Main class for reading in and parsing contigs"""
-    def __init__(self, contigsFile):
-        self.contigsFile = contigsFile
-
-    def parse(self, dbName):
+        # make sure we're only overwriting existing DBs with the users consent
         try:
-            with open(dbName) as f: pass
-        except IOError as e:
-            print "Error: database:",dbName,"does not exist - perhaps you should call the bamParser first"
-        
-        return 3
-
-class Counter:
-    """Callback for counting aligned reads"""
-    counts = 0
-    def __call__(self, alignment):
-        self.counts += 1
-           
-class BamParser:
-    """Parse multiple bam files and write the output to hdf5 """
-
-    def __init__(self, bamFiles):
-        self.bamFiles = bamFiles[0].split(',')
-        self.numBamFiles = len(self.bamFiles)
-        self.bamColNames = []
-        
-    def getBamDescriptor(self, fullPath):
-        """reduce a full path to just the file name minus extension"""
-        return os.path.splitext(os.path.basename(fullPath))[0]
-    
-    def parse(self, dbName):
-        """ parse multiple bam files and store the results in the main DB"""
-        # make sure we'r eonly overwriting existing DBs with the users consent
-        try:
-            with open('filename') as f:
-                option = raw_input("Warning: database:",dbName,"exists. Overwrite? (y,n)")
+            with open(dbName) as f:
+                option = raw_input("Warning: database: "+dbName+" exists. Overwrite? (y,n)")
                 if(option.upper() != "Y"):
                     print "Operation cancelled"
                     return False
@@ -157,89 +98,276 @@ class BamParser:
                     print "Overwriting database",dbName
         except IOError as e:
             print "Creating new database", dbName
-            
-        # parse the BAMs
-        tmp_storage = {}
-        bam_count = 0
-        try:
-            for bf in self.bamFiles:
-                bam_file = None
-                try:
-                    bam_file = pysam.Samfile(bf, 'rb')
-                except:
-                    print "Unable to open BAM file",bf,"-- did you supply a SAM file instead?"
-                    return False
-
-                try:
-                    for reference, length in zip(bam_file.references, bam_file.lengths):
-                        c = Counter()
-                        try:
-                            bam_file.fetch(reference, 0, length, callback = c )
-                            num_reads = c.counts
-                        except ValueError as e:
-                            print "Could not calculate num reads for:",reference,"in",bf,"\t",e
-                            return False
-                        except:
-                            print "Could not calculate num reads for:",reference,"in",bf, sys.exc_info()[0]
-                            return False
-                        
-                        this_bf = self.getBamDescriptor(bf)
-                        
-                        # save this guy into the tmp_storage
-                        if(reference not in tmp_storage):
-                            tmp_storage[reference] = np.zeros((self.numBamFiles+1))
-                            tmp_storage[reference][self.numBamFiles] = length
-                        # we need to divide the count by the length if we are going
-                        # to use a normalised coverage
-                        tmp_storage[reference][bam_count] = float(num_reads)/float(length)
-                except:
-                    print "Error parsing bam file:",bf, sys.exc_info()[0]
-                    return False
-                bam_count += 1
-        except:
-            print "Error parsing bam files:", sys.exc_info()[0]
-            return False
-
-        # create the db and write results
-        # build a table template based on the number of bamfiles we have
-        db_desc = { 'cid' : tables.StringCol(64) }
-        for bf in self.bamFiles:
-            # assume the file is called somethign like "fred.bam"
-            # we want to rip off the ".bam" part
-            bam_desc = self.getBamDescriptor(bf)
-            db_desc[bam_desc] = tables.FloatCol()
-            self.bamColNames.append(bam_desc)
-        db_desc['length'] = tables.FloatCol()
+            raise
         
+        # create the db
         try:        
             with tables.openFile(dbName, mode = "w", title = "GroopM") as h5file:
                 # Create a new group under "/" (root)
                 group = h5file.createGroup("/", 'profile', 'Assembly profiles')
-                # Create one table on it
-                table = h5file.createTable(group, 'coverage', db_desc, "Bam based coverage")
-                # go through all the contigs sorted by name
-                for cid in sorted(tmp_storage.keys()):
-                    # make a new row
-                    cov_row = table.row
-                    # punch in the data
-                    cov_row['cid'] = cid
-                    for i in range(0,len(self.bamColNames)):
-                        cov_row[self.bamColNames[i]] = tmp_storage[cid][i]
-                    cov_row['length'] = tmp_storage[cid][self.numBamFiles]
-                    cov_row.append()
-                table.flush()
+
+                #------------------------
+                # parse bam files
+                #------------------------
+                # build a table template based on the number of bamfiles we have
+                db_desc = { 'cid' : tables.StringCol(64) }
+                for bf in self.bamFiles:
+                    # assume the file is called somethign like "fred.bam"
+                    # we want to rip off the ".bam" part
+                    bam_desc = getBamDescriptor(bf)
+                    db_desc[bam_desc] = tables.FloatCol()
+                    self.bamColNames.append(bam_desc)
+                db_desc['length'] = tables.FloatCol()
+                try:
+                    cov_table = h5file.createTable(group, 'coverage', db_desc, "Bam based coverage")
+                    self.bamParser.parse(cov_table, self.bamFiles, self.bamColNames)
+                except:
+                    print "Error creating coverage table:", sys.exc_info()[0]
+                    raise
+                
+                #------------------------
+                # parse contigs 
+                #------------------------
+                db_desc = { 'cid' : tables.StringCol(64) }
+                for mer in self.kse.kmerCols:
+                     db_desc[mer] = tables.FloatCol()
+                try:
+                    TM_table = h5file.createTable(group, 'tns', db_desc, "TetraNucl signature")
+                except:
+                    print "Error creating TNS table:", sys.exc_info()[0]
+                    raise
+                try:
+                    f = open(self.contigsFile, "r")
+                    self.numContigs = self.conParser.parse(f, self.kse, TM_table)
+                    f.close()
+                except:
+                    print "Could not parse contig file:",self.contigsFile,sys.exc_info()[0]
+                    raise
+
+                #------------------------
+                # parse aux profile 
+                #------------------------
+                
         except:
-            print "Error saving results to DB"
+            print "Error creating database:", dbName, sys.exc_info()[0]
+            raise
+        
+        if(self.numContigs <= 0):
+            print "Something went wrong while parsing contigs"
             return False
 
+        with tables.openFile(dbName, mode = "r") as h5file:
+            self.bamParser.dumpCovTable(h5file.root.profile.coverage, self.bamColNames)
+            self.conParser.dumpTnTable(h5file.root.profile.tns, self.kse.kmerCols)
+            
         return True
 
-    def dumpCovTable(self, dbName):
-        with tables.openFile(dbName, mode = "r") as h5file:
-            table = h5file.root.profile.coverage
-            for row in table:
-                print row['cid'],",",
-                for i in range(0,len(self.bamColNames)):
-                    print row[self.bamColNames[i]],",",
-                print row['length']
+###############################################################################
+# CONTIG PARSING CLASSES
+###############################################################################
+class ContigParser:
+    """Main class for reading in and parsing contigs"""
+    def __init__(self): pass
 
+    def readfq(self, fp): # this is a generator function
+        """https://github.com/lh3"""
+        last = None # this is a buffer keeping the last unprocessed line
+        while True: # mimic closure; is it a bad idea?
+            if not last: # the first record or a record following a fastq
+                for l in fp: # search for the start of the next record
+                    if l[0] in '>@': # fasta/q header line
+                        last = l[:-1] # save this line
+                        break
+            if not last: break
+            name, seqs, last = last[1:].split()[0], [], None
+            for l in fp: # read the sequence
+                if l[0] in '@+>':
+                    last = l[:-1]
+                    break
+                seqs.append(l[:-1])
+            if not last or last[0] != '+': # this is a fasta record
+                yield name, ''.join(seqs), None # yield a fasta record
+                if not last: break
+            else: # this is a fastq record
+                seq, leng, seqs = ''.join(seqs), 0, []
+                for l in fp: # read the quality
+                    seqs.append(l[:-1])
+                    leng += len(l) - 1
+                    if leng >= len(seq): # have read enough quality
+                        last = None
+                        yield name, seq, ''.join(seqs); # yield a fastq record
+                        break
+                if last: # reach EOF before reading enough quality
+                    yield name, seq, None # yield a fasta record instead
+                    break
+                
+    def parse(self, contigFile, kse, table):
+        """Do the heavy lifting of parsing"""
+        num_contigs = 0
+        for cid,seq,qual in self.readfq(contigFile):
+            num_contigs += 1
+            sig = kse.getKSig(seq)
+            # make a new row
+            TN_row = table.row
+            # punch in the data
+            TN_row['cid'] = cid
+            for mer in sig.keys():
+                TN_row[mer] = sig[mer]
+            TN_row.append()
+        table.flush()
+        return num_contigs
+
+    def dumpTnTable(self, table, kmerCols):
+        """dump the guts of the TN table"""
+
+        print "-----------------------------------"
+        print "TNuclSig table"
+        print "-----------------------------------"
+        for row in table:
+            print row['cid'],
+            for mer in kmerCols:
+                print ",",row[mer],
+            print ""
+
+class KmerSigEngine:
+    """Simple class for determining kmer signatures"""
+
+    def __init__(self, kLen):
+        self.kLen = kLen
+        self.compl = string.maketrans('ACGT', 'TGCA')
+        self.kmerCols = self.makeKmerColNames()
+        self.numMers = len(self.kmerCols)
+        
+    def makeKmerColNames(self):
+        """work out the range of kmers required based on kmer length"""
+        # build up the big list
+        base_words = ("A","C","G","T")
+        out_list = ["A","C","G","T"]
+        for i in range(1,self.kLen):
+            working_list = []
+            for mer in out_list:
+                for char in base_words:
+                    working_list.append(mer+char)
+            out_list = working_list
+        
+        # pear it down based on lexicographical ordering
+        ret_list = []
+        for mer in out_list:
+            lmer = self.shiftLowLexi(mer)
+            if lmer not in ret_list:
+                ret_list.append(lmer)
+        return ret_list
+    
+    def shiftLowLexi(self, seq):
+        """return the lexicographically lowest form of this sequence"""
+        rseq = self.revComp(seq)
+        if(seq < rseq):
+            return seq
+        return rseq
+        
+    def revComp(self, seq):
+        """return the reverse complement of a sequence"""
+        return seq.translate(self.compl)[::-1]
+    
+    def getKSig(self, seq):
+        """Work out kmer signature for a nucleotide sequence"""
+        sig = dict(zip(self.kmerCols, [0.0] * self.numMers))
+        ll = len(seq)
+        for i in range(0,ll-self.kLen+1):
+            sig[self.shiftLowLexi(seq[i:i+self.kLen])] += 1
+        # normalise by length and return
+        return dict(zip(self.kmerCols, [ X / ll for X in sig.values()]))
+
+
+###############################################################################
+# BAM PARSING CLASSES
+###############################################################################
+class BamParser:
+    """Parse multiple bam files and write the output to hdf5 """
+
+    def __init__(self): pass
+    
+    def parse(self, table, bamFiles, bamColNames):
+        """ parse multiple bam files and store the results in the main DB
+        
+        table: a table in an open h5 file like "CID,COV_1,...,COV_n,length"
+        bamColNames: names of the COV_x columns
+        """
+        
+        # parse the BAMs
+        tmp_storage = {}
+        bam_count = 0
+        for bf in bamFiles:
+            bam_file = None
+            try:
+                bam_file = pysam.Samfile(bf, 'rb')
+                self.parseBam(bam_file, bam_count, len(bamColNames), tmp_storage)                
+                bam_count += 1
+            except:
+                print "Unable to open BAM file",bf,"-- did you supply a SAM file instead?"
+                raise
+
+        # go through all the contigs sorted by name and write to the DB
+        try:
+            for cid in sorted(tmp_storage.keys()):
+                # make a new row
+                cov_row = table.row
+                # punch in the data
+                cov_row['cid'] = cid
+                for i in range(0,len(bamColNames)):
+                    cov_row[bamColNames[i]] = tmp_storage[cid][i]
+                cov_row['length'] = tmp_storage[cid][len(bamColNames)]
+                cov_row.append()
+            table.flush()
+        except:
+            print "Error saving results to DB"
+            raise
+
+    def parseBam(self, bamFile, bamCount, numBams, storage):
+        """Parse the bam file handle and store the number of reads mapped"""
+        
+        for reference, length in zip(bamFile.references, bamFile.lengths):
+            c = Counter()
+            try:
+                bamFile.fetch(reference, 0, length, callback = c )
+                num_reads = c.counts
+            except ValueError as e:
+                print "Could not calculate num reads for:",reference,"in",bf,"\t",e
+                raise
+            except:
+                print "Could not calculate num reads for:",reference,"in",bf, sys.exc_info()[0]
+                raise
+            
+            # save this guy into the tmp_storage
+            if(reference not in storage):
+                storage[reference] = np.zeros((numBams+1))
+                storage[reference][numBams] = length
+
+            # we need to divide the count by the length if we are going
+            # to use a normalised coverage
+            storage[reference][bamCount] = float(num_reads)/float(length)
+        
+    def dumpCovTable(self, table, bamColNames):
+        """dump the guts of the coverage table"""
+
+        print "-----------------------------------"
+        print "Coverage table"
+        print "-----------------------------------"
+        for row in table:
+            print row['cid'],",",
+            for colName in bamColNames:
+                print row[colName],",",
+            print row['length']
+
+class Counter:
+    """Call back for counting aligned reads
+    
+    Used in conjunction with pysam.fetch 
+    """
+    counts = 0
+    def __call__(self, alignment):
+        self.counts += 1
+
+def getBamDescriptor(fullPath):
+    """reduce a full path to just the file name minus extension"""
+    return os.path.splitext(os.path.basename(fullPath))[0]
