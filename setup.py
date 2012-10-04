12de0fee053e31a4521bf96a3bf4cba2dbd45085
Michael.Imelfort <mike@mikeimelfort.com>
2012-10-04 13:23:12 +1000

    oops

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='GroopM',
-    version='0.0.1',
+    version='0.0.1.1',
     author='Michael Imelfort',
     author_email='mike@mikeimelfort.com',
     packages=['groopm', 'groopm.test'],
