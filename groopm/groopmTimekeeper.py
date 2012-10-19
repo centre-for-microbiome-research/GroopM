184a7404fe3b7e43b9513d66ef21074e85a78063
Michael.Imelfort <mike@mikeimelfort.com>
2012-10-19 23:47:29 +1000

    Wrap up the timing functions in one spot

---
+++ groopm/groopmTimekeeper.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+###############################################################################
+#                                                                             #
+#    groopmTimekeeper.py                                                      #
+#                                                                             #
+#    Class for creating time stamps                                           #
+#                                                                             #
+#    Copyright (C) Michael Imelfort                                           #
+#                                                                             #
+###############################################################################
+#                                                                             #
+#          .d8888b.                                    888b     d888          #
+#         d88P  Y88b                                   8888b   d8888          #
+#         888    888                                   88888b.d88888          #
+#         888        888d888 .d88b.   .d88b.  88888b.  888Y88888P888          #
+#         888  88888 888P"  d88""88b d88""88b 888 "88b 888 Y888P 888          #
+#         888    888 888    888  888 888  888 888  888 888  Y8P  888          #
+#         Y88b  d88P 888    Y88..88P Y88..88P 888 d88P 888   "   888          #
+#          "Y8888P88 888     "Y88P"   "Y88P"  88888P"  888       888          #
+#                                             888                             #
+#                                             888                             #
+#                                             888                             #
+#                                                                             #
+###############################################################################
+#                                                                             #
+#    This program is free software: you can redistribute it and/or modify     #
+#    it under the terms of the GNU General Public License as published by     #
+#    the Free Software Foundation, either version 3 of the License, or        #
+#    (at your option) any later version.                                      #
+#                                                                             #
+#    This program is distributed in the hope that it will be useful,          #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of           #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            #
+#    GNU General Public License for more details.                             #
+#                                                                             #
+#    You should have received a copy of the GNU General Public License        #
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.     #
+#                                                                             #
+###############################################################################
+
+__author__ = "Michael Imelfort"
+__copyright__ = "Copyright 2012"
+__credits__ = ["Michael Imelfort"]
+__license__ = "GPL3"
+__version__ = "0.0.1"
+__maintainer__ = "Michael Imelfort"
+__email__ = "mike@mikeimelfort.com"
+__status__ = "Development"
+
+###############################################################################
+import time
+
+###############################################################################
+###############################################################################
+###############################################################################
+###############################################################################
+
+class TimeKeeper:
+    def __init__(self):
+        self.startTime = time.time()
+        self.lastLogTime = self.startTime 
+    
+    def startTimer(self):
+        """Restart the timer"""
+        self.startTime = time.time()
+        self.lastLogTime = self.startTime 
+
+    def getTimeStamp(self):
+        """Make a time stamp"""
+        now = time.time()
+        ret_str = "THIS: [ %s ]\tTOTAL: [ %s ]" % (self.secondsToStr(now - self.lastLogTime), self.secondsToStr(now - self.startTime))
+        self.lastLogTime = now
+        return ret_str  
+        
+    def secondsToStr(self, t):
+        rediv = lambda ll,b : list(divmod(ll[0],b)) + ll[1:]
+        return "%d:%02d:%02d.%03d" % tuple(reduce(rediv,[[t*1000,],1000,60,60]))
+    
+###############################################################################
+###############################################################################
+###############################################################################
+###############################################################################
